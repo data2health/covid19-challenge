17476941a1a0ac3ec1e4590dea2c7b4616d435c2
Thomas Yu <thomasyu888@gmail.com>
2020-05-28 10:06:23 -0700

    Update code

--- scripts/add_rank.py
+++ scripts/add_rank.py
@@ -12,25 +12,17 @@ def cli():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('evaluationid', type=int,
                         help='A question number to add')
+    parser.add_argument('--username', type=str,
+                        help='Synapse Username')
+    parser.add_argument('--password', type=str,
+                        help="Synapse Password")
     args = parser.parse_args()
     return args
 
 
-def main():
-    """Main"""
-    args = cli()
-    syn = synapseclient.login()
-    queue_id = args.evaluationid
-
-    queue_mapping = syn.tableQuery(
-        f"SELECT * FROM syn22077175 where main = '{queue_id}'"
-    )
-    queue_mappingdf = queue_mapping.asDataFrame()
-    if queue_mappingdf.empty:
-        raise ValueError("No queue id")
-    # TODO: use site record here to get dataset version
-    internal_queue = queue_mappingdf['internal'].iloc[0]
-    # TODO: Need to support multiple internal queue rankings
+def get_score_results(syn, main_queue, internal_queue, site=None):
+    """Get score results for site"""
+    print("Getting score results")
     dataset_mapping = syn.tableQuery(
         f"SELECT * FROM syn22093564 where queue = '{internal_queue}'"
     )
@@ -43,29 +35,59 @@ def main():
     train = dataset_info['train_dataset_version']
     infer = dataset_info['infer_dataset_version']
     query = (
-        f"select * from evaluation_{queue_id} where "
-        f"UW_train_dataset_version == '{train}' and "
-        f"UW_infer_dataset_version == '{infer}' and "
-        "UW_submission_status == 'SCORED'"
+        f"select * from evaluation_{main_queue} where "
+        f"{site}_train_dataset_version == '{train}' and "
+        f"{site}_infer_dataset_version == '{infer}' and "
+        f"{site}_submission_status == 'SCORED'"
     )
     results = list(evaluation_queue_query(syn, query))
     resultsdf = pd.DataFrame(results)
+    return resultsdf
+
+
+def add_rank(syn, resultsdf, site, score_column):
+    """Add ranking to submission status"""
+    print("Adding rank")
     resultsdf['ranking'] = -1
-    sorted_values = resultsdf.sort_values("UW_PRAUC", ascending=False)
+    sorted_values = resultsdf.sort_values(score_column, ascending=False)
     nodups = sorted_values.drop_duplicates("submitterId")
 
-    resultsdf.loc[nodups.index, 'ranking'] = nodups['UW_PRAUC'].rank(
+    resultsdf.loc[nodups.index, 'ranking'] = nodups[score_column].rank(
         ascending=False, method='min'
     )
-    resultsdf['ranking'] = resultsdf['ranking'].astype(int) 
-    for index, row in resultsdf.iterrows():
-        print(row['objectId'])
+    resultsdf['ranking'] = resultsdf['ranking'].astype(int)
+
+    for _, row in resultsdf.iterrows():
+        # print(row['objectId'])
         sub_status = syn.getSubmissionStatus(row['objectId'])
         updated = update_single_submission_status(
-            sub_status, {'ranking': row['ranking']},
+            sub_status, {f"{site}_rank": row['ranking']},
             is_private=False
         )
         syn.store(updated)
+
+
+def main():
+    """Main"""
+    args = cli()
+    syn = synapseclient.Synapse()
+    syn.login(email=args.username, password=args.password)
+    queue_id = args.evaluationid
+    # TODO: Can loop through main queues instead of specifying
+    # a queue
+    queue_mapping = syn.tableQuery(
+        f"SELECT * FROM syn22077175 where main = '{queue_id}'"
+    )
+    queue_mappingdf = queue_mapping.asDataFrame()
+    if queue_mappingdf.empty:
+        raise ValueError("No queue id")
+    queue_mapping_dict = queue_mappingdf.to_dict('records')
+    for queue_map in queue_mapping_dict:
+        internal_queue = queue_map['internal']
+        site = queue_map['site']
+        resultsdf = get_score_results(syn, queue_id, internal_queue, site)
+        # TODO: Need to support different score columns per site
+        add_rank(syn, resultsdf, site, "UW_PRAUC")
 
 
 if __name__ == "__main__":
